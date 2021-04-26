f13359a2f6e2803d01de115b19edecb44262db69
WenYHsieh <48989331+WenYHsieh@users.noreply.github.com>
2021-04-26 20:57:01 +0800

    revised

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,7 +1,6 @@
 function reverse(str) {
     let result = [];
     for (let i=str.length-1; i>=0; i--){
-        // 依序叫號: 長度-1開始, >=0符合的話，每一輪印出來後減1 
         result.push(str[i]);
     }
     result = result.join('');
