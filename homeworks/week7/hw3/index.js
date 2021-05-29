50594daf4cab4cd441c6c68e5f33d640eaa3ea7f
WenYHsieh <48989331+WenYHsieh@users.noreply.github.com>
2021-05-30 01:07:19 +0800

    hw3 revision 0530

--- homeworks/week7/hw3/index.js
+++ homeworks/week7/hw3/index.js
@@ -1,27 +1,29 @@
 const inputToDo = document.querySelector('input[type="text"]')
 const todoWrapper = document.querySelector('.todo-wrapper')
-let todos = []
+let todos = {}
 
-function render() { // 清空, 為每個 todo 都產生相對應的 html elements
+function render() {
   todoWrapper.innerHTML = ''
-  todos.forEach((newToDo, id) => {
-    todoWrapper.innerHTML += `<br><div class="todo-container todo-${id}">
-                              <input type="checkbox" id="checkbox-no${id}">
-                              <label for="checkbox-no${id}">${newToDo}</label>
-                              <label class="delete del-${id}"></label>
+  const arrToDos = Object.entries(todos)
+  arrToDos.forEach((newToDoArr) => {
+    todoWrapper.innerHTML += `<div class="todo-container todo-${newToDoArr[0]}">
+                              <input type="checkbox" id="checkbox-no${newToDoArr[0]}">
+                              <label for="checkbox-no${newToDoArr[0]}">${newToDoArr[1]}</label>
+                              <label class="delete del-${newToDoArr[0]}"></label>
                               </div>`
-    const newToDoContainer = document.querySelector(`.todo-${id}`)
+    const newToDoContainer = document.querySelector(`.todo-${newToDoArr[0]}`)
     newToDoContainer.style.position = 'relative'
   })
   const newDelBtns = document.querySelectorAll('.delete')
   newDelBtns.forEach((newDelBtn) => monitorDel(newDelBtn))
 }
 
-function monitorAdd() { // 如果按下 enter 且有輸入值 => 取輸入值，存到 todos，把 input 清空，更新畫面
+function monitorAdd() {
   inputToDo.addEventListener('keydown',
     (e) => {
       if (e.keyCode === 13 && inputToDo.value !== '') {
-        todos.push(inputToDo.value)
+        const nextId = Object.keys(todos).unshift() + 1
+        todos[nextId] = inputToDo.value
         inputToDo.value = ''
         render()
       }
@@ -29,20 +31,26 @@ function monitorAdd() { // 如果按下 enter 且有輸入值 => 取輸入值，
   )
 }
 
-function monitorDel(newDelBtn) { // 如果按下刪除鍵 => 得到刪除鍵的 ID, 刪除那個位置的 todo, 更新畫面
+function monitorDel(newDelBtn) {
   newDelBtn.addEventListener('click',
     (e) => {
       const delId = e.target.getAttribute('class').split(' ')[1].split('-')[1]
-      todos.splice(parseInt(delId), 1)
-      render()
+      const delToDoContainer = document.querySelector(`.todo-${delId}`)
+      delToDoContainer.remove()
+      delete todos[delId]
     }
   )
 }
 
 function init() {
+  todos = {
+    1: 'play with cats',
+    2: 'report revision',
+    3: 'visit dentist',
+    4: 'clean desktop',
+    5: 'take packages'
+  }
   monitorAdd()
-  todos = ['clean desktop', 'meet milu', 'play with cats',
-    'report revision', 'Take the package', 'visit dentist']
   render()
 }
 
