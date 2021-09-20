e38c6fee8271f15f8b4ed583a6b0715e46fe5804
謝文瑜 <wenyu@xiewenyudeMacBook-Air.local>
2021-09-20 16:37:03 +0800

    0920 first deploy

---
+++ homeworks/week21/hw2/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Game from './Game'
+import Board from "./components/Board"
+
+ReactDOM.render(
+  <Board />,
+  document.getElementById('root')
+);
